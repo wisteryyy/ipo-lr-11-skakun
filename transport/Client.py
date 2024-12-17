4a32d12be7126c90ec61ef036b1f97823dcc601b
Sofia <sofiaskakun30@gmail.com>
2024-12-17 10:25:39 +0300

    Client.py

--- transport/Client.py
+++ transport/Client.py
@@ -1,11 +1,11 @@
 class Client: # определяем класс Client, который представляет клиента компании
     def __init__(self, name, cargo_weight, is_vip=False): # конструктор класса, принимающий имя, вес груза и VIP-статус (по умолчанию False)
-        if not name or not name.strip():
-            raise ValueError("Имя клиента не может быть пустой строкой.")
-        
-        if not isinstance(cargo_weight, (int, float)) or cargo_weight < 0:
-            raise ValueError("Вес груза должен быть положительным числом.")
-        
+        if not name.isalpha() or len(name) < 2: # проверяем, что имя состоит только из букв и длина не менее 2
+            raise ValueError("Имя клиента должно содержать только буквы и быть не менее 2 символов.")
+
+        if not isinstance(cargo_weight, (float, int)) or cargo_weight <= 0 or cargo_weight > 10000: # проверяем, что вес положительный и не превышает 10000
+            raise ValueError("Вес груза должен быть положительным числом и не более 10000 кг.")
+
         if not isinstance(is_vip, bool):
             raise ValueError("VIP-статус должен быть True или False.")
         
